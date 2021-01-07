1ba0b803b38ab199d637b3964eda3f0c5cd7a179
xuyuejun-hci <mfm19201@bene.fit.ac.jp>
2021-01-07 16:42:34 +0900

    Add push button test scene

---
+++ Assets/Script/InputTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    void OnNewAction()
+    {
+        Debug.Log("push button");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
