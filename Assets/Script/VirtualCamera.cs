53ed89266ac226c55d80c15a67e4648457aacb25
Natchapon <natchaponrakcharoen@gmail.com>
2021-01-21 14:44:30 +0900

    Add random speed function and fixed bug

--- Assets/Script/VirtualCamera.cs
+++ Assets/Script/VirtualCamera.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 //using UnityEngine.Experimental.Input;
 public class VirtualCamera : MonoBehaviour
 {
     public Transform Camera;
+    public GameObject CavasText;
+
     public float CameraRotationY;
     public float HumanRotationY;
     public int Direct;
     public float lastValue;
     public int state = 0;
-    public int EnableChange = 0;
-    public int LastEnableChange = 0;
-    public float Gain = 0.5f;
-    public float AutoRotateDirection = 0;
-    public float y = 0;
-
+    int EnableChange = 0;
+    int LastEnableChange = 0;
+    float Gain = 0.5f;
+    float AutoRotateDirection = 0;
+    float y = 0;
+    float Speed ;
+    bool StartState = false;
     // Start is called before the first frame update
     void OnRotateLeft()
     {
@@ -24,25 +28,41 @@ public class VirtualCamera : MonoBehaviour
     void OnRotateRight()
     {
            AutoRotateDirection = 1;
+ 
+    }
+    void OnStartProgram()
+    {
+           StartState = true;
+           CavasText.gameObject.SetActive(false);
     }
 
     void Start()
     {
-       
+        RandomSpeed();       
+        Debug.Log("Speed:" + Speed);
+
     }
     // Update is called once per frame
     void Update()
     {
-        //CameraRotationY = Camera.localEulerAngles.y;
-        
-        CameraRotationY = AutoRotateConstantSpeed();
-        HumanRotationY = CameraRotationY;
-        SetDirection();    
-        CheckAndUpDate_State();
-        RotateCam();            
-        Camera.transform.eulerAngles = new Vector3(0,CameraRotationY,0);
-        transform.eulerAngles = new Vector3(0f, HumanRotationY, 0.0f); 
-        lastValue = CameraRotationY;
+        if(StartState == true)
+        {
+            //CameraRotationY = Camera.localEulerAngles.y;
+            CameraRotationY = AutoRotateConstantSpeed();
+            HumanRotationY = CameraRotationY;
+            SetDirection();    
+            CheckAndUpDate_State();
+            RotateCam();            
+            transform.eulerAngles = new Vector3(0f, HumanRotationY, 0.0f); 
+            Camera.transform.eulerAngles = new Vector3(0,CameraRotationY,0);
+
+            lastValue = CameraRotationY;
+        }
+    }
+    void RandomSpeed()
+    {
+        int RanD = Random.Range(0,7);
+        Speed = Mathf.Pow(2,RanD)/10;
     }
     float AutoRotateConstantSpeed()
     {
@@ -50,22 +70,22 @@ public class VirtualCamera : MonoBehaviour
         {
             if(y < 360 && y >0  )
             {
-                y += Time.deltaTime * 30;
+                y += Time.deltaTime * Speed;
             }
             else
             {
-            y = 0 +Time.deltaTime * 30;
+            y = 0 +Time.deltaTime * Speed;
             }
         }
         else if(AutoRotateDirection == 1)
         {
             if(y < 360 && y > 0 )
             {
-                y -= Time.deltaTime * 30;
+                y -= Time.deltaTime * Speed;
             }
             else
             {
-            y = 360 - Time.deltaTime * 30;
+            y = 360 - Time.deltaTime * Speed;
             }
         }
         return y;
@@ -74,11 +94,11 @@ public class VirtualCamera : MonoBehaviour
     void SetDirection()
     {
         float tmp = CameraRotationY - lastValue;
-        if(tmp > 0.15  )
+        if(tmp > 0  )
         {
            Direct = 1;
         }
-        else if(tmp < -0.15)
+        else if(tmp < 0 )
         {
            Direct = 2;
         }
@@ -116,7 +136,7 @@ public class VirtualCamera : MonoBehaviour
         }
         else  if(Direct == 2){
 
-            if( HumanRotationY < 90 && LastEnableChange != 1)
+            if( HumanRotationY < 90  && LastEnableChange != 1)
             {
                 state -= 1;
                 EnableChange = 1;
